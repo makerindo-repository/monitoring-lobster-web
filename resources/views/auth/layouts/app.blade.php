f251a113cfb446335555b4e59c15e6455b150cd9
fahrifuh <muhammadfahri571@gmail.com>
2025-10-23 09:52:11 +0700

    refactor: ubah icon di tab menjadi logo unpad

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="icon" href="{{asset('images/ipb-logo.png')}}">
+    <link rel="icon" href="{{asset('images/unpad-logo.png')}}">
 
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
--- resources/views/pages/layouts/app.blade.php
+++ resources/views/pages/layouts/app.blade.php
@@ -92,7 +92,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'WQMCS') }}</title>
-    <link rel="icon" href="{{asset('images/ipb-logo.png')}}">
+    <link rel="icon" href="{{asset('images/unpad-logo.png')}}">
 
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
