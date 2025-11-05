5b44df3b50cafbcd39fdb8e7bc31a66a1009b783
fahrifuh <muhammadfahri571@gmail.com>
2025-11-05 10:04:40 +0700

    feat: membuat migration dan model untuk tabel kamera

---
+++ database/migrations/2025_11_05_091001_create_cameras_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCamerasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cameras', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_kamera');
+            $table->foreignId('kja_id')->constrained('kja', 'id')->onDelete('cascade');
+            $table->string('ip_kamera');
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cameras');
+    }
+}
