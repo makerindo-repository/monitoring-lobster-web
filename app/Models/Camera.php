31c860d6c5255216c24a360be96f6ff9590691a8
fahrifuh <muhammadfahri571@gmail.com>
2025-11-06 09:48:47 +0700

    feat: menambah relasi

--- app/Models/Camera.php
+++ app/Models/Camera.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Kja;
 
 class Camera extends Model 
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function kja()
+    {
+        return $this->belongsTo(Kja::class, 'kja_id');
+    }
 }
--- app/Models/Kja.php
+++ app/Models/Kja.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Camera;
 
 class Kja extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
     protected $table = 'kja';
+
+    public function cameras()
+    {
+        return $this->hasMany(Camera::class);
+    }
 }
