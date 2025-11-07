1200959e00aff7fe19d6657c8c9a72084fd8cf32
fahrifuh <muhammadfahri571@gmail.com>
2025-11-07 09:42:46 +0700

    feat: membuat model dan migration untuk tabel petugas

---
+++ app/Petugas.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Petugas extends Model
+{
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function pakan()
+    {
+        return $this->hasMany(LogPakan::class);
+    }
+}
